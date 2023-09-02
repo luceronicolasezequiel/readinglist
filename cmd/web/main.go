571ed182abc40ae398b135a0dfc9801f0ff7fe08
luceronicolasezequiel <lucero.nicolas.ezequiel@gmail>
2023-09-02 10:35:14 -0300

    INI-0001 Creating the Reading List Model

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,15 +4,21 @@ import (
 	"flag"
 	"log"
 	"net/http"
+
+	"readinglist.luceronicolasezequiel/internal/models"
 )
 
 type application struct {
+	readinglist *models.ReadinglistModel
 }
 
 func main() {
 	addr := flag.String("addr", ":80", "HTTP network address")
+	endpoint := flag.String("endpoint", "http://localhost:4000/v1/books", "Endpoint for the reading list web service")
 
-	app := &application{}
+	app := &application{
+		readinglist: &models.ReadinglistModel{Endpoint: *endpoint},
+	}
 
 	srv := &http.Server{
 		Addr:    *addr,
